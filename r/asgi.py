dd26ffe156a8dc328464dca11aa91192d1618ddc
balakrishna88 <42884357+balakrishna88@users.noreply.github.com>
2025-02-16 16:01:03 +0530

    Update asgi.py

--- r/asgi.py
+++ r/asgi.py
@@ -1,14 +1,21 @@
-# asgi.py
+# r/asgi.py
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat import routing  # Import your WebSocket routing
 
+# Set the default settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'r.settings')
 
+# Initialize Django
+django_asgi_app = get_asgi_application()
+
+# Import routing AFTER Django is initialized
+from chat import routing  # noqa: E402 (disable import order warning)
+
+# Define the ASGI application
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
